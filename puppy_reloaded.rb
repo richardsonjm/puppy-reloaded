3ea2f05f8e6eb1a37e1533b99627edd8cfcf498f
Daniel Chang <daniel.chang85@gmail.com>
2013-10-11 21:04:18 -0400

    html copy

---
+++ puppy_reloaded.rb
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width">
+    <title>1000 (1600×1000)</title>
+    <style>[touch-action="none"]{ -ms-touch-action: none; touch-action: none; }[touch-action="pan-x"]{ -ms-touch-action: pan-x; touch-action: pan-x; }[touch-action="pan-y"]{ -ms-touch-action: pan-y; touch-action: pan-y; }[touch-action="scroll"],[touch-action="pan-x pan-y"],[touch-action="pan-y pan-x"]{ -ms-touch-action: pan-x pan-y; touch-action: pan-x pan-y; }</style>
+  </head>
+
+  <body style="margin: 0px;">
+
+    <img style="-webkit-user-select: none; cursor: -webkit-zoom-out;" src="http://placedog.com/1600/1000" width="1600" height="1000">
+
+  </body>
+</html>
