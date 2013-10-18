3bd93efd0e6f83ab9bd00b43f0be94f052fc961a
Daniel Chang <daniel.chang85@gmail.com>
2013-10-17 22:00:04 -0400

    Deleted extra files. Created .gitignore in site dir. Renamed runner.

--- bin/runner.rb
+++
@@ -1,3 +0,0 @@
-require_relative "../config/environment.rb"
-
-puppies = Runner.new
--- lib/models/site_generator.rb
+++ lib/models/site_generator.rb
@@ -1,6 +1,4 @@
-class Runner
-  puts Dir.pwd
-  binding.pry
+class SiteGenerator
   settings = Reload.new("W@i† fo4r 1T...", 1200, 1000, 5)
   index = ERB.new(File.open('../lib/views/puppy_reloaded.erb').read)
   File.open('../_site/index.html', 'w+') do |f|
