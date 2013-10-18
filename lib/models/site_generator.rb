d31b4b3c9781d4a1fe6cc18fa7435fc541eb6d2d
Daniel Chang <daniel.chang85@gmail.com>
2013-10-17 22:16:04 -0400

    small change

--- lib/models/site_generator.rb
+++ lib/models/site_generator.rb
@@ -1,4 +1,4 @@
-class Runner
+class SiteGenerator
   settings = Reload.new("W@i† fo4r 1T...", 1200, 1000, 5)
   index = ERB.new(File.open('../lib/views/puppy_reloaded.erb').read)
   File.open('../_site/index.html', 'w+') do |f|
